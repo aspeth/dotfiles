80cd79c7b4d7f69406db2bb9da10e146b96ea05c
Andrew Speth <andrew.speth@locushealth.com>
2024-04-29 08:52:02 -0600

    move wezterm config to home directory

---
+++ dot_wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = {}
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 18
+
+config.color_scheme = "Night Owl (Gogh)"
+
+config.keys = {
+	{
+		key = "d",
+		mods = "SUPER",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "w",
+		mods = "SUPER",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "ALT|SUPER",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT|SUPER",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "UpArrow",
+		mods = "ALT|SUPER",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "DownArrow",
+		mods = "ALT|SUPER",
+		action = act.ActivatePaneDirection("Down"),
+	},
+}
+
+return config
